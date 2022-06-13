363da478e8c545f17f744003775759f5b7df4da3
任东升 <ren690612@qq.com>
2022-06-13 20:15:06 +0800

    initial commit

---
+++ build/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js'
+  },
+  resolve: {
+    // 自动解析文件后缀拓展
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    // 配置指定后缀名文件的处理
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  devtool: process.env.NODE_ENV === 'production' ? false: 'inline-source-map',
+  devServer: {
+    static: ['./dist'],    // 本地运行时基于的文件目录
+    compress: false,
+    host: 'localhost',
+    port: 8089
+  },
+  stats: 'errors-only',
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['./dist']
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/template/index.html'
+    })
+  ]
+}
