7056780fc600f55989190ebc86e95bd37d039d06
ren.r.2 <ren.r.2@pg.com>
2022-06-14 16:18:28 +0800

    修改注释

--- src/example/basic-type.ts
+++ src/example/basic-type.ts
@@ -1,31 +1,39 @@
-// 1. 布尔类型
-
+/**1. 布尔类型
+ */
 // let bool: boolean = false
 let bool: boolean
 bool = true
 // bool = 123    // Type 'number' is not assignable to type 'boolean'
+console.log('---------------------------------------------------------------')
 
-// 2. 数值类型
+/**2. 数值类型
+ */
 let num: number = 123     // 十进制
 num = 0b111011            // 二进制
 num = 0o173               // 八进制
 num = 0x7b                // 十六进制
 num = NaN 
+console.log('---------------------------------------------------------------')
 
-// 3. 字符串类型
+/**3. 字符串类型
+ */
 let str: string
 str = 'abc'
 str = `数值是${num}`
+console.log('---------------------------------------------------------------')
 
-// 4. 数组类型
+/**4. 数组类型
+ */
 let arr1: number[]  // 写法一
 arr1 = [1, 2, 3]
 
 let arr2: Array<number> // 写法二
 // let arr3: [number | string]
 // let arr3: (number | string)[]
+console.log('---------------------------------------------------------------')
 
-// 5. 元组类型 -- 数组形式且必须按照类型顺序
+/**5. 元组类型 -- 数组形式且必须按照类型顺序
+ */
 let tuple: [string, number, boolean]
 
 // 枚举类型 -- 只能用 const 不可以用 let 
@@ -55,23 +63,31 @@ const enum PostStatus {
 
 // console.log(Roles.SUPER_ADMIN, PostStatus.UnPublished)
 // console.log(0 /* Roles.SUPER_ADMIN */, 7 /* PostStatus.UnPublished */);
+console.log('---------------------------------------------------------------')
 
-// any 类型
+/**6. any 类型
+ */
 let values: any
+console.log('---------------------------------------------------------------')
 
-// void 类型 -- 什么类型都不是
+/**7. void 类型 -- 什么类型都不是
+ */
 const consoleText = (text: string):void => {
   console.log(text)
 }
 let v:void 
 v = undefined
 // v = null
+console.log('---------------------------------------------------------------')
 
-// null和undefined -- 是其他类型的子类型
+/**8. null和undefined -- 是其他类型的子类型
+ */
 let u: undefined = void 0
 let n: null = null
+console.log('---------------------------------------------------------------')
 
-// never类型
+/**9. never类型
+ */
 // 抛错
 const errorFunc = (message: string): never => {
   throw new Error(message)
@@ -80,13 +96,17 @@ const errorFunc = (message: string): never => {
 const infiniteFunc = (): never => {
   while(true) {}
 }
+console.log('---------------------------------------------------------------')
 
-// object
+/**10. object
+ */
 function getObject(obj: Object): void {
   console.log(obj)
 }
+console.log('---------------------------------------------------------------')
 
-// 类型断言
+/**11. 类型断言
+ */
 const getLength = (target: string | number): number => {
   // 两种方式, 但可以看到每一次使用都要写一次, 非常繁琐
   // 在 jsx 中只能使用 as, 不能使用 <> 的方式(像一个标签)
